ec9212de98303a9508249dc955053b8b18447097
Jack Hawkins <jhawkins__5@outlook.com>
2024-11-02 14:46:29 +0000

    Update formToJSON.js

--- formToJSON.js
+++ formToJSON.js
@@ -1,47 +1,49 @@
-// identify the form and load it
-const form = document.querySelector('#form');
-console.log(String(form))
+// identify the form and load it when the document has loaded
+window.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('form');
+    console.log(form)
 
-// set error messages for validation
-const RATING_REQUIRED = "Please enter a rating.";
-const REVIEW_REQUIRED = "Please enter a review.";
+    // set error messages for validation
+    const RATING_REQUIRED = "Please enter a rating.";
+    const REVIEW_REQUIRED = "Please enter a review.";
 
-function hasValue(input, message) {
-    // if the input is empty, show an error
-    if (input.value.trim() === "") {
-        alert(message);
+    function hasValue(input, message) {
+        // if the input is empty, show an error
+        if (input.value.trim() === "") {
+            alert(message);
+        }
     }
-}
 
-form.addEventListener('submit', function (event) {
-    // stop the form from submitting
-    event.preventDefault();
+    form.addEventListener('submit', function (event) {
+        // stop the form from submitting
+        event.preventDefault();
+        var object = {};
 
-    // load the values from the form and save their values
-    const name = form.elements[0];
-    let fullName = name.value;
-    const organisation = form.elements[1];
-    let org = organisation.value;
-    const roomType = form.elements[2];
-    let room = roomType.value;
-    const rating = hasValue(form.elements[3], RATING_REQUIRED);
-    let rate = rating.value;
-    const reviewString = hasValue(form.elements[4], REVIEW_REQUIRED);
-    let review = reviewString.value;
-    console.log('executing')
-    // if the rating and review are not null, map each value to a dictionary
-//    if (!(rate.value == null || review.value == null)) {
-//        var object = {};
+        // load the values from the form and map their values
+        const name = form.elements[0];
+        let fullName = name.value;
+        object['fullName'] = fullName;
+        const organisation = form.elements[1];
+        let org = organisation.value;
+        object['organisation'] = org;
+        const roomType = form.elements[2];
+        let room = roomType.value;
+        object['roomType'] = room;
+        //const rating = hasValue(form.elements[3], RATING_REQUIRED);
+        //let rate = rating.value;
+        const reviewString = hasValue(form.elements[3], REVIEW_REQUIRED);
+
+        // return if the review value is empty
+        if (reviewString == "") {
+            return;
+        }
+
+        let review = reviewString.value;
+        object['review'] = review;
         
-//        FormData.forEach(function(value, key){
-//            object[key] = value;
-//        });
-
-        // turn the dictionary into JSON
+        // convert the data to JSON
         var json = JSON.stringify(object);
         console.log(json);
         return json;
-//    }
-
-    return null;
-});
+    });
+})
